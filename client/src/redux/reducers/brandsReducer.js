2cc4dcfeaa9e67aeca7067540bb07c1f8978e21e
Filipp <philipp3gs@gmail.com>
2022-09-30 21:35:22 +0300

    add community only hardcod

---
+++ client/src/redux/reducers/brandsReducer.js
@@ -0,0 +1,9 @@
+/* eslint-disable default-param-last */
+export default function brandsReducer(state = [], action) {
+  const { type, payload } = action;
+  switch (type) {
+    // добавить CASE
+    default:
+      return state;
+  }
+}
