fd7b88e9c80a5fca8ab889fb7d5eb3e85aa9bd7b
Filipp <philipp3gs@gmail.com>
2022-09-30 15:52:53 +0300

    navbar done

--- client/src/components/UI/Navbar/Navbar.jsx
+++ client/src/components/UI/Navbar/Navbar.jsx
@@ -1,11 +1,106 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import AdbIcon from '@mui/icons-material/Adb';
+import { NavLink } from 'react-router-dom';
 
-function Navbar() {
+function ResponsiveAppBar() {
   return (
-    <div>
-      Навбар
-    </div>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            AutoForum 21
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            AutoForum
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block' }}
+              component={NavLink}
+              to="/registration"
+            >
+              Регистрация
+            </Button>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block' }}
+              component={NavLink}
+              to="/login"
+            >
+              Авторизация
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Перейти в личный кабинет">
+              <IconButton component={NavLink} to="/users/:userId" sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/elDar.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Button
+              sx={{ color: 'white' }}
+              component={NavLink}
+              to="/login"
+            >
+              Выйти
+            </Button>
+
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
-export default Navbar;
+export default ResponsiveAppBar;
