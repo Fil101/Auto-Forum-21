7f6268faf57206e1270bae82a97c3d9f87a97248
Vladimir Cherkasov <retal13@rambler.ru>
2022-09-30 19:14:22 +0300

    seed brand

--- server/db/seeders/20220930133754-car_brand.js
+++ server/db/seeders/20220930133754-car_brand.js
@@ -1,24 +1,33 @@
 'use strict';
+const brendTopArr = [
+  'Audi','BMW','Chery',
+  'Chevrolet','Citroen','Daewoo',
+  'EXEED','Ford','Geely',
+  'Haval','Honda','Hyundai',
+  'Infiniti','Jeep','Kia',
+  'LADA (ВАЗ)','Land-Rover','Lexus',
+  'Mazda','Mercedes-Benz',
+  'Mitsubishi','Nissan','Opel',
+  'Peugeot','Porsche','Renault',
+  'Skoda','Subaru','Suzuki',
+  'Toyota','Volkswagen',
+  'Volvo','ГАЗ','УАЗ'];
 
 module.exports = {
+  
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
+      await queryInterface.bulkInsert('Car_brands', brendTopArr.map((brand)=>({
+        name: brand,
+        logo:`/Logo/${brand}.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })), {},);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+ 
+      await queryInterface.bulkDelete('Car_brands', null, {});
+    
   }
 };
