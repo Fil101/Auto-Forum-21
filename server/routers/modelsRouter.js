8b1a0a5f5d499549fb5c6cae092846ab2f4bcb09
Filipp <philipp3gs@gmail.com>
2022-10-03 14:42:49 +0300

    add modal window post

--- server/routers/modelsRouter.js
+++ server/routers/modelsRouter.js
@@ -6,7 +6,7 @@ const { Car_model } = require('../db/models');
 const router = express.Router();
 
 router.get('/all', async (req, res) => {
-  const { modelId } = req.params;
+  const { brandId } = req.params;
   console.log('\x1b[34m%s\x1b[0m', 'Сработала ручка Models');
   const models = await Car_model.findAll({ raw: true });
   // console.log(models);
