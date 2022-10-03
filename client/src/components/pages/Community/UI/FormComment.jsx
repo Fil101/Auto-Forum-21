9186489c7129d22433d6c58f86ccaa6215f5ec48
Filipp <philipp3gs@gmail.com>
2022-10-03 15:51:28 +0300

    fix registration

--- client/src/components/pages/Community/UI/FormComment.jsx
+++ client/src/components/pages/Community/UI/FormComment.jsx
@@ -1,12 +1,31 @@
 import { DateRange, Image } from '@mui/icons-material';
-import { Box, Button, ButtonGroup, Stack, TextField } from '@mui/material';
+import { Box, Button, ButtonGroup, Stack, TextField, Typography } from '@mui/material';
 
 import React from 'react';
 
 function FormComment(props) {
   return (
-    <Box type="form">
-      Hello
+    <Box
+      type="form"
+      sx={{
+        width: '80%',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography variant="h6" color="gray" textAlign="center">
+        Add Commit
+      </Typography>
+      <TextField
+        sx={{ width: '100%' }}
+        id="standard-multiline-static"
+        multiline
+        rows={3}
+        placeholder="Title"
+        variant="standard"
+        name="title"
+      />
+      <Button type="submit">Commit</Button>
     </Box>
   );
 }
